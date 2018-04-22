2eb031d55d0e1b6c4615a7b85966d9d29848f9cd
sharqawi-web <38073623+sharqawi-web@users.noreply.github.com>
2018-04-22 14:15:58 +0200

    Update akbaar.js

--- akbaar.js
+++ akbaar.js
@@ -65,7 +65,7 @@ function recent_post_content(){
     s += "أخر المواضيع";
     s += "</div>";
     }
-    s += "  <marquee style='float:left; margin-left:10px; width:82%' scrollAmount='"+scrolling_speed+"'>";
+    s += "  <marquee style='float:right; margin-right:10px; width:82%' scrollAmount='"+scrolling_speed+"'>";
     for(var i=0; i<latest_post; i++){
         var recent_post_entries = entries[i];
         s += "<a href='"+recent_post_entries.href+"' ";
@@ -76,7 +76,7 @@ function recent_post_content(){
     s += "</marquee>";
     s += "</div>";
     if(close_button){
-	s += "<div style='float:right;margin-right:15px;'>";
+	s += "<div style='float:left;margin-left:15px;'>";
     s += "<a href='javascript:void(0)' onclick='document.getElementById(\"recent_post\").style.display=\"none\"'>";
     s += "(x)";
     s += "</a>";
